0e6fc762cc5b846afd48a9167d47de2da8e13bf2
Sanil Davis <sanil.davis@speehive.com>
2022-02-13 14:35:50 +0530

    Add project files.

---
+++ 2ndByKzWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _2ndByKzWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
